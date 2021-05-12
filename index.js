1f1e4fb726d12c5b073f34024c93625ceadc5dd9
dakshsethi <sethidaksh02@gmail.com>
2021-05-12 23:58:54 +0530

    Test 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const data = require('./routes/data');
 const express = require('express');
 const app = express();
--- routes/data.js
+++ routes/data.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Data!!');
+    // console.log(`${process.env.PORT}`);
 });
+
+router.get('/:id', (req, res) => {
+    res.send(req.params.id);
+})
 
 
 
