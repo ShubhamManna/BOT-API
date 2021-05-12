f0c8af352dc8c640c7d54a14ffe7166a1457d177
dakshsethi <sethidaksh02@gmail.com>
2021-05-12 22:54:53 +0530

    Test 1

---
+++ index.js
@@ -0,0 +1,33 @@
+const data = require('./routes/data');
+const express = require('express');
+const app = express();
+app.use(express.json()); //Adding Middleware and using that Middleware
+app.use(express.urlencoded({ extended: true }));  //Can send key=value&key=value
+
+app.use(function(req, res, next) {
+    console.log('Logging...');
+    next();
+});
+
+app.use(function(req, res, next) {
+    console.log('Authenticating...');
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/data', data);
+
+// app.get('/data', (req, res) => {
+//     res.send('Getting Data...');
+// })
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
