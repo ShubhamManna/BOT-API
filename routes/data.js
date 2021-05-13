d9c64096acd94348dcc29958ca5e0ee3baa284aa
dakshsethi <sethidaksh02@gmail.com>
2021-05-14 01:02:27 +0530

    Some minor fixes

--- routes/data.js
+++ routes/data.js
@@ -1,40 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
 const {Firestore} = require('@google-cloud/firestore');
-
-// Create a new client
-// const firestore = new Firestore();
-// import "firebase/firestore";
-// var firebase = require('firebase/app');
-// const db = require('firebase/firestore');
-// const firebaseConfig = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.FIREBASE_APP_ID
-// };
-  
-//   // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
-
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("path/to/serviceAccountKey.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
-
 const admin = require('firebase-admin');
 const serviceAccount = require('./../ServiceAccountKey.json');
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
 const db = admin.firestore();
 
 // gettodo().then(result => {
@@ -57,8 +30,6 @@ router.get('/todo/', (req, res) => {
     db.collection('todo').orderBy('timestamp').get().then((querySnapshot) => {
         const todoList = [];
         querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            // console.log(doc.id, " => ", doc.data());
             todoList.push(doc.data());
         });
         res.send(todoList);
@@ -77,29 +48,8 @@ router.get('/todo/:date', (req, res) => {
     });
 });
 
-/*
-To test this GoTo Postman -> New Request -> enter localhost:3000/api/courses and select POST
-Now Goto Body -> raw -> select JSON -> type {"name" : **** } and hit SEND
-*/
-// app.post('/api/courses', (req, res) => {
-
-//     const { error } = validateCourse(req.body); //result.error
-//     if(error) return res.status(400).send(error.details[0].message);
-
-//     // if(req.body.name || req.body.name.length < 3) {
-//     //     //400 Bad Request
-//     //     res.status(400).send('Name is required and should be minimum 3 characters long!!');
-//     //     return ;
-//     // }
-//     const course = {
-//         id: courses.length + 1,
-//         name: req.body.name //Enable Parsing of JSON objects
-//     };
-//     courses.push(course);
-//     res.send(courses);
-// })
-
 router.post('/todo/', (req, res) => {
+
     //for Error -->  return res.status(400).send("Error Occured")
     if(req.body.message == '') return res.status(400).send("Error Occured!!");
     const todo = {
@@ -108,6 +58,7 @@ router.post('/todo/', (req, res) => {
         message: req.body.message,
         checked: false
     }
+    console.log(todo.date);
 
     db.collection("todo").add(todo);
     console.log('Task Added Successfully!!');
@@ -116,7 +67,7 @@ router.post('/todo/', (req, res) => {
 
 router.put('/todo/:id', (req, res) => {
     const todoList = [];
-    db.collection('todo').where('date', '==', '13-05-2021').orderBy('timestamp').get().then((querySnapshot) => {
+    db.collection('todo').where('date', '==', `${dd_mm_yyyy()}`).orderBy('timestamp').get().then((querySnapshot) => {
         let temp = {};
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
